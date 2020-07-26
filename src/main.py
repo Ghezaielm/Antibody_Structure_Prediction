12de3eff13b58e9356bafb90d59e340bafce43e6
Ghezaielm <55768957+Ghezaielm@users.noreply.github.com>
2020-07-26 16:13:51 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,7 @@
+from init import launch
 
+paths = ["4ydi.pdb","4ydj.pdb","4ydk.pdb","4ydl.pdb","4ydv.pdb"]
+init = launch(paths)
+init._parse()
+init._process()
+init._Model(100,100,bs=200,epochs=100)
