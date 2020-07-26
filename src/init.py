f9eef65fce3a61d0319f6f0f35848ee55b24396f
Ghezaielm <55768957+Ghezaielm@users.noreply.github.com>
2020-07-26 16:07:39 +0200

    Update init.py

--- src/init.py
+++ src/init.py
@@ -1,1 +1,39 @@
+from monitor import monitor
 
+
+class launch(monitor): 
+    
+    def __init__(self,path): 
+        self.paths = path
+    
+    @monitor.timer
+    def _parse(self): 
+        self.parsed = []
+        for structure in self.paths:
+            o = parseStructure(structure)
+            o.getStructure()
+            o.rotateStructure()
+            o.getFeatures()
+            for parsed in o.data:
+                self.parsed.append(parsed)
+
+    @monitor.timer        
+    def _process(self):
+        o = processData(self.parsed)
+        o.addBcmFeatures()
+        o.addPolyFeatures()
+        o.slice()
+        self.X = o.X
+        self.y = o.y
+        print("Input shape: {}\n Output shape: {}".format(o.X.shape,o.y.shape))
+    
+    def _Model(self,l1,l2,epochs=5,bs=10):
+        o = LSTM_AE(self.X,self.y)
+        o.splitData()
+        o.setModel(l1,l2)
+        o.train(epochs,bs)
+        o.showMetrics("Run 1")
+        o.fineTune(epochs=50,bs=200)
+        o.showMetrics("Fine tuning")
+        self.result = o.copy()
+        
