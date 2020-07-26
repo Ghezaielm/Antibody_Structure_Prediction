a86e408120a8d8f95921e3cdc63dd016b979d495
Ghezaielm <55768957+Ghezaielm@users.noreply.github.com>
2020-07-26 16:09:24 +0200

    Update monitor.py

--- src/monitor.py
+++ src/monitor.py
@@ -1,1 +1,29 @@
+##### Monitor methods s##################################
+# Methods: timer (wrapper to monitor execution time)
+########################################################
 
+class monitor():
+    
+    def __init__(self): 
+        pass
+    
+    @staticmethod
+    def timer(func):           
+        def wrapper(self,*args,**kwargs):
+            start = time.time()
+            
+            print('\n')
+            print("###",func.__name__)
+            func(self,*args,**kwargs)
+            print("Elapsed time = {} seconds".format(round(time.time()-start)))
+            print('\n')
+        return wrapper
+    
+    @staticmethod
+    def memoryUsage(func):           
+        def wrapper(self):
+            func(self)
+            process = psutil.Process(os.getpid())
+            print("% memory: {}".format(round(process.memory_percent(),2)),"\n")
+        return wrapper
+  
